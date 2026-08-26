e5f26703f356cd3d60279e4b802d1395508b8c4e
TheDirepack <48868531+TheDirepack@users.noreply.github.com>
2026-08-26 08:30:59 -0400

    test(readiness): cover unencrypted acknowledgement gate

---
+++ tests/nixos/invalid/installation-ready-unencrypted.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }:
+
+{
+  nas = {
+    installationReady = lib.mkForce true;
+    testing.installationReadyFixture = true;
+    trustedInterfaces = lib.mkForce [ "eth0" ];
+    adminPasswordHashFile = lib.mkForce (toString (pkgs.writeText "negative-admin-password-hash" "!"));
+    zfsEncryption.acknowledgeUnencrypted = lib.mkForce false;
+  };
+}
